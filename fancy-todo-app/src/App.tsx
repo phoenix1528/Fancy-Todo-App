5cdc5573b33b278f27017f9d26e8580323a952b7
unknown <steffl.phoenix@gmx.at>
2021-02-07 15:28:37 +0100

    added bootstrap and icon for todo list

--- fancy-todo-app/src/App.tsx
+++ fancy-todo-app/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import toDoList from './to-do-list.svg';
 import axios from 'axios';
 
 interface IValue {
@@ -17,6 +17,7 @@ export default class App extends Component {
     axios
       .get('https://localhost:44326/api/WeatherForecast/values')
       .then((response) => {
+        console.log(response);
         this.setState({
           values: response.data,
         });
@@ -25,18 +26,86 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <ul>
-            {this.state.values.map((value: IValue) => (
-              <li>
-                {value.id}:{value.name}
-              </li>
-            ))}
+      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+        <a
+          className='navbar-brand medium-icon'
+          href='http://localhost:8000/test'
+        >
+          <img src={toDoList} className='medium-icon' alt='logo' />
+        </a>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarSupportedContent'
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+
+        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+          <ul className='navbar-nav mr-auto'>
+            <li className='nav-item'>
+              <a className='nav-link' href='#'>
+                Home <span className='sr-only'>(current)</span>
+              </a>
+            </li>
+            <li className='nav-item'>
+              <div>
+                Icons made by{' '}
+                <a href='https://www.freepik.com' title='Freepik'>
+                  Freepik
+                </a>{' '}
+                from{' '}
+                <a href='https://www.flaticon.com/' title='Flaticon'>
+                  www.flaticon.com
+                </a>
+              </div>
+            </li>
+            <li className='nav-item dropdown'>
+              <a
+                className='nav-link dropdown-toggle'
+                href='#'
+                id='navbarDropdown'
+                role='button'
+                data-toggle='dropdown'
+                aria-haspopup='true'
+                aria-expanded='false'
+              >
+                Dropdown
+              </a>
+              <div className='dropdown-menu' aria-labelledby='navbarDropdown'>
+                <a className='dropdown-item' href='#'>
+                  Action
+                </a>
+                <a className='dropdown-item' href='#'>
+                  Another action
+                </a>
+                <div className='dropdown-divider'></div>
+                <a className='dropdown-item' href='#'>
+                  Something else here
+                </a>
+              </div>
+            </li>
           </ul>
-        </header>
-      </div>
+          <form className='form-inline my-2 my-lg-0'>
+            <input
+              className='form-control mr-sm-2'
+              type='search'
+              placeholder='Search'
+              aria-label='Search'
+            />
+            <button
+              className='btn btn-outline-success my-2 my-sm-0'
+              type='submit'
+            >
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
     );
   }
 }
